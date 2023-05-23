0dcacdb454458f4a0df097864dd4e0f0c72af15d
furkan <furka@DESKTOP-TRR628R>
2023-05-23 20:17:26 +0300

    Add project files.

---
+++ MySkill/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MySkill.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
